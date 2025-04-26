b4deacf7f85c88c4030074f8de11cc4a81ed70b5
Najmul Haque <93752166+Stevenhaque@users.noreply.github.com>
2025-04-26 18:34:28 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    bool isFreqSame(int freq[], int freq2[]) {
+        for(int i = 0; i < 26; i++) {
+            if(freq[i] != freq2[i]) { 
+                return false;
+            }
+        }
+        return true;
+    }
+
+    bool checkInclusion(string s1, string s2) {
+        int freq[26] = {0};
+
+        for(int i = 0; i < s1.length(); i++) { 
+            freq[s1[i] - 'a']++;
+        }
+
+        int windSize = s1.length(); 
+
+        for(int i = 0; i <= s2.length() - windSize; i++) { 
+            int windFreq[26] = {0};
+
+            for(int j = 0; j < windSize; j++) { 
+                windFreq[s2[i + j] - 'a']++;
+            }
+
+            if (isFreqSame(freq, windFreq)) {
+                return true;
+            }
+        }
+
+        return false; 
+    }
+};
